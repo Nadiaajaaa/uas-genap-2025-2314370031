b6e729460b18fd0603f47ca22df146ebc2d78e25
Nadiaaajaaa <nadiakirani17@icloud.com>
2025-07-20 20:11:21 +0700

    perbaiki case sensitivity (lagi)

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
+use App\Models\category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\View\View;
